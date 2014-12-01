b034ae7c079ccf8396af02203c2eb865d226d4e1
peteflorence <pete.florence@gmail.com>
2014-12-01 18:34:54 -0500

    adding in yalmip

---
+++ yalmip/Contents.m
@@ -0,0 +1,19 @@
+% YALMIP
+% Version 25-Nov-2014
+%
+% Information
+%
+% Variables.
+%   sdpvar       - Create SDPVAR variable 
+%   ...find out more in the Wiki!
+
+% Inequalites and constraints.
+%   ...check the Wiki
+%
+% Optimization related.
+%   solvesdp     - Computes solution to optimization problem
+%   ...
+%
+
+% Author Johan Löfberg
+
